50e3799ac4d2f7383475fcca3da93737c42894d3
Ackrome <ivantishchenko59@gmail.com>
2025-03-03 19:41:47 +0300

    some changes

--- app/keyboards.py
+++ app/keyboards.py
@@ -12,7 +12,7 @@ help = ReplyKeyboardMarkup(
 
 
 submodules = ReplyKeyboardMarkup(
-    keyboard=[[KeyboardButton(text=i)] for i in matplobblib.submodules],
+    keyboard=[[KeyboardButton(text=i)] for i in matplobblib.submodules + commands],
     resize_keyboard=True,
     input_field_placeholder='Что выберем, хозяин?',
     one_time_keyboard=True
@@ -22,13 +22,13 @@ topics_dict = dict()
 for el in range(len(matplobblib.submodules)):
     module = matplobblib._importlib.import_module(f'matplobblib.{matplobblib.submodules[el]}')
     topics_dict[matplobblib.submodules[el]] = [ReplyKeyboardMarkup(
-        keyboard=[[KeyboardButton(text=i)] for i in list(module.themes_list_dicts_full.keys())],
+        keyboard=[[KeyboardButton(text=i)] for i in list(module.themes_list_dicts_full.keys()) + commands],
         resize_keyboard=True,
         input_field_placeholder='Что выберем, хозяин?',
         one_time_keyboard=True
     ),
     {key: ReplyKeyboardMarkup(
-        keyboard=[[KeyboardButton(text=i)] for i in list(module.themes_list_dicts_full[key].keys())],
+        keyboard=[[KeyboardButton(text=i)] for i in list(module.themes_list_dicts_full[key].keys()) + commands],
         resize_keyboard=True,
         input_field_placeholder='Что выберем, хозяин?',
         one_time_keyboard=True
--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import os
 import threading
 from dotenv import load_dotenv
 from flask import Flask
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, BaseMiddleware
+from aiogram.types import Update
 
 from app.handlers import router
 
@@ -12,6 +13,21 @@ logging.basicConfig(level=logging.INFO)
 load_dotenv()
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
+
+# Middleware для логгирования информации о пользователе
+class UserLoggingMiddleware(BaseMiddleware):
+    async def __call__(self, handler, event: Update, data):
+        user_info = "Unknown user"
+        if event.message:
+            user = event.message.from_user
+            user_info = f"{user.full_name} (@{user.username})" if user.username else user.full_name
+        elif event.callback_query:
+            user = event.callback_query.from_user
+            user_info = f"{user.full_name} (@{user.username})" if user.username else user.full_name
+        logging.info(f"Получено обновление от пользователя: {user_info}")
+        return await handler(event, data)
+
+
 PORT = int(os.getenv("PORT", 5000))
 
 # Создаем Flask-приложение
@@ -25,6 +41,7 @@ def index():
 async def main():
     bot = Bot(BOT_TOKEN)
     dp = Dispatcher()
+    dp.update.middleware(UserLoggingMiddleware())
     dp.include_router(router)
     await dp.start_polling(bot)
 
