70175a31bd584be5b16b4eb8fbd70bdf84b77fa1
Ackrome <ivantishchenko59@gmail.com>
2025-03-03 18:57:12 +0300

    minor changes

--- app/handlers.py
+++ app/handlers.py
@@ -48,43 +48,60 @@ class Search(StatesGroup):
 @router.message(Command('ask'))
 async def ask(message: Message, state: FSMContext):
     await state.set_state(Search.submodule)
-    await message.answer('Введите ваш вопрос',
-                         reply_markup = kb.submodules
-                         )
-    
+    await message.answer('Введите ваш вопрос', reply_markup=kb.submodules)
+
 @router.message(Search.submodule)
-async def ask(message: Message, state: FSMContext):
+async def process_submodule(message: Message, state: FSMContext):
+    # Проверяем, что введённый подмодуль является ожидаемым
+    if message.text not in matplobblib.submodules:
+        await message.answer("Неверный выбор. Попробуйте еще раз.", reply_markup=kb.submodules)
+        return
     await state.update_data(submodule=message.text)
+    # Импортируем модуль для получения списка тем
+    module = matplobblib._importlib.import_module(f'matplobblib.{message.text}')
+    topics = list(module.themes_list_dicts_full.keys())
     await state.set_state(Search.topic)
-    await message.answer('Введите тему',
-                         reply_markup=kb.topics_dict[message.text][0]
-                         )
+    await message.answer("Введите тему", reply_markup=kb.topics_dict[message.text][0])
 
 @router.message(Search.topic)
-async def ask(message: Message, state: FSMContext):
-    await state.update_data(topic=message.text)
+async def process_topic(message: Message, state: FSMContext):
     data = await state.get_data()
+    submodule = data["submodule"]
+    module = matplobblib._importlib.import_module(f'matplobblib.{submodule}')
+    topics = list(module.themes_list_dicts_full.keys())
+    # Если тема не входит в ожидаемые, просим попробовать снова
+    if message.text not in topics:
+        await message.answer("Неверный выбор. Попробуйте еще раз.", reply_markup=kb.topics_dict[submodule][0])
+        return
+    await state.update_data(topic=message.text)
     await state.set_state(Search.code)
-    await message.answer('Выберите задачу',
-                         reply_markup=kb.topics_dict[data["submodule"]][1][data["topic"]]
-                         )
+    await message.answer("Выберите задачу", reply_markup=kb.topics_dict[submodule][1][message.text])
 
 @router.message(Search.code)
-async def ask(message: Message, state: FSMContext):
+async def process_code(message: Message, state: FSMContext):
+    data = await state.get_data()
+    submodule = data["submodule"]
+    topic = data["topic"]
+    module = matplobblib._importlib.import_module(f'matplobblib.{submodule}')
+    possible_codes = list(module.themes_list_dicts_full[topic].keys())
+    # Если выбранная задача не входит в ожидаемые, просим повторить выбор
+    if message.text not in possible_codes:
+        await message.answer("Неверный выбор. Попробуйте еще раз.", reply_markup=kb.topics_dict[submodule][1][topic])
+        return
     await state.update_data(code=message.text)
     data = await state.get_data()
-    await message.answer(f'Ваш запрос: \n {data["submodule"]} \n {data["topic"]} \n {data["code"]}')
-    repl = matplobblib._importlib.import_module(f'matplobblib.{data["submodule"]}').themes_list_dicts_full[data["topic"]][data["code"]]
+    await message.answer(f'Ваш запрос: \n{submodule} \n{topic} \n{data["code"]}')
+    repl = module.themes_list_dicts_full[topic][data["code"]]
     if len(repl) > 4096:
         await message.answer('Сообщение будет отправлено в нескольких частях')
         for x in range(0, len(repl), 4096):
             await message.answer(f'''```python\n{repl[x:x+4096]}\n```''',
-                         parse_mode='markdown',
-                         reply_markup=kb.help)
+                                 parse_mode='markdown',
+                                 reply_markup=kb.help)
     else:
         await message.answer(f'''```python\n{repl}\n```''',
-                         parse_mode='markdown',
-                         reply_markup=kb.help)
+                             parse_mode='markdown',
+                             reply_markup=kb.help)
     await state.clear()
 ##################################################################################################
 # UPDATE
--- main.py
+++ main.py
@@ -8,6 +8,14 @@ from aiogram import Bot, Dispatcher, F
 
 from app.handlers import router
 # from app.database.models import async_main
+from flask import Flask
+
+fl = Flask(__name__)
+
+@fl.route('/')
+def index():
+    return "Hello, Render.com!"
+
 
 logging.basicConfig(level=logging.INFO)
 load_dotenv()
@@ -25,6 +33,11 @@ async def main():
     await dp.start_polling(bot)
     
 if __name__ == '__main__':
+    try:
+        port = int(os.getenv("PORT"))
+        fl.run(host="0.0.0.0", port=port)
+    except:
+        pass
     try:
         asyncio.run(main())
     except (KeyboardInterrupt, SystemExit):
