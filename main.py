7bafeb10e77010873313ed50885c96b60f9bfc25
Ackrome <ivantishchenko59@gmail.com>
2025-03-03 19:19:58 +0300

    fixed flask

--- main.py
+++ main.py
@@ -1,44 +1,44 @@
 import asyncio
 import logging
-from dotenv import load_dotenv
 import os
-
-from aiogram import Bot, Dispatcher, F
-
+import threading
+from dotenv import load_dotenv
+from flask import Flask
+from aiogram import Bot, Dispatcher
 
 from app.handlers import router
-# from app.database.models import async_main
-from flask import Flask
 
+logging.basicConfig(level=logging.INFO)
+load_dotenv()
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+PORT = int(os.getenv("PORT", 5000))
+
+# Создаем Flask-приложение
 fl = Flask(__name__)
 
 @fl.route('/')
 def index():
     return "Hello, Render.com!"
 
-
-logging.basicConfig(level=logging.INFO)
-load_dotenv()
-
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-
-
-
+# Асинхронная функция для запуска бота
 async def main():
-    # async_main()
-    
     bot = Bot(BOT_TOKEN)
     dp = Dispatcher()
     dp.include_router(router)
     await dp.start_polling(bot)
-    
+
+# Функция для запуска Flask-сервера
+def run_flask():
+    fl.run(host="0.0.0.0", port=PORT)
+
 if __name__ == '__main__':
+    # Запускаем Flask в отдельном потоке
+    flask_thread = threading.Thread(target=run_flask, daemon=True)
+    flask_thread.start()
+    
     try:
-        port = int(os.getenv("PORT"))
-        fl.run(host="0.0.0.0", port=port)
-    except:
-        pass
-    try:
+        # Запускаем асинхронного бота в основном потоке
         asyncio.run(main())
     except (KeyboardInterrupt, SystemExit):
         logging.error('Bot stopped!')
