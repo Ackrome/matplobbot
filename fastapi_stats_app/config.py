492e0360ac9b9e2171c1c52b64977b7fecc9ee7b
Ackrome <ivantishchenko59@gmail.com>
2025-10-22 22:59:03 +0300

    Fixed BD_PATH in api config file

--- fastapi_stats_app/config.py
+++ fastapi_stats_app/config.py
@@ -1,5 +1,5 @@
 # c:/Users/ivant/Desktop/proj/matplobbot/fastapi_stats_app/config.py
-DB_PATH = "/app/db/user_actions.db"
+DB_PATH = "/app/db_data/user_actions.db"
 LOG_DIR = "/app/logs"  # Общая директория для логов
 BOT_LOG_FILE_NAME = "bot.log" # Имя файла логов бота
 FASTAPI_LOG_FILE_NAME = "fastapi_app.log" # Имя файла логов FastAPI
