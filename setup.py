8df2d90e3b45f2d26d91da3f37d7b1d3a322b4b6
Ackrome <ivantishchenko59@gmail.com>
2025-11-01 23:23:23 +0300

    shared_lib...

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="matplobbot-shared",  # The name you use to `pip install`
-    version="0.1.23",
-    packages=['shared_lib', 'shared_lib.services', 'shared_lib.locales'], # Explicitly list packages
+    name="matplobbot-shared",
+    version="0.1.20", # Let's use the version from your requirements.txt
+    packages=find_packages(include=['shared_lib', 'shared_lib.*']),
     description="Shared library for the Matplobbot ecosystem (database, services, i18n).",
     author="Ackrome",
     author_email="ivansergeyevich@gmail.com",
     # Declare dependencies for this library
     install_requires=[
-        "asyncpg>=0.30.0",
-        "aiohttp>=3.13.2", # Specify versions as needed
-        "certifi>=2025.10.5"
+        "asyncpg",
+        "aiohttp", # Specify versions as needed
+        "certifi"
     ],
     # This tells setuptools that the package data (like .json files) should be included
     package_data={
